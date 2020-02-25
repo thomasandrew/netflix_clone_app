2f10dfc438d4e174f34c6046dcf1e6be34dc9cfc
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-02-24 22:59:07 -0300

    Update Play.js

--- components/Play.js
+++ components/Play.js
@@ -13,46 +13,45 @@ import { Icon } from 'react-native-elements'
 export default class App extends Component {
     render() {
         return (
-           
-                <View style={styles.play}>
-                <TouchableOpacity style={{left: 10}}>
-                   <View style={styles.plus}>
+            <View style={styles.play}>
+                <TouchableOpacity style={{ left: 10 }}>
+                    <View style={styles.plus}>
                         <Icon type='ionicon' name='md-add' size={30} color="#fff" />
                     </View>
                     <View>
                         <Text style={styles.my}>My List</Text>
                     </View>
-                </TouchableOpacity>    
-
+                </TouchableOpacity>
                 <View>
                     <TouchableOpacity style={styles.btn_play}>
-                       <View style={{marginLeft: -50, marginTop: 7}}>
-                            <Icon type='font-awesome' name='play' size={20} color='#181818' /> 
+                        <View style={{ marginLeft: -50, marginTop: 7 }}>
+                            <Icon type='font-awesome' name='play' size={20} color='#181818' />
                         </View>
-                         
-                        <View style={{marginLeft: 40, bottom: 19}}>
-                            <Text style={{fontFamily: 'Verdana'}}>
-                               Play
+                        <View style={{ marginLeft: 40, bottom: 19 }}>
+                            <Text style={{ fontFamily: 'Verdana' }}>
+                                Play
                             </Text>
-                        </View> 
-                        
+                        </View>
                     </TouchableOpacity>
-                    
-                </View> 
-                 
-
-
-
                 </View>
-            
+                <View style={styles.info}>
+                    <TouchableOpacity style={{ width: 50, height: 50 }}>
+                        <View style={{ bottom: 3 }}>
+                            <Icon type='font-awesome' name='info-circle' size={27} color='#fff' />
+                        </View>
+                        <View style={{ left: 14, bottom: 4 }}>
+                            <Text style={{ color: '#fff' }}>Info</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
     play: {
-        marginTop: 270,
-       
+        marginTop: 277,
     },
     plus: {
         marginLeft: -200,
@@ -70,6 +69,9 @@ const styles = StyleSheet.create({
         marginTop: -40,
         backgroundColor: '#fff',
     },
-
+    info: {
+        left: 190,
+        bottom: 41,
+    },
 })
 
