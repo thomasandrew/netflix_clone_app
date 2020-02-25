a79393613284581dd2131bed9b54d9d08d02daf8
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-02-25 14:36:16 -0300

    Update Movies.js

--- components/Movies.js
+++ components/Movies.js
@@ -14,27 +14,36 @@ export default class App extends Component {
     render() {
         return (
             <View style={styles.movie}>
-              {/*  <View>
-                    <Text>
-                       Popular on Netflix
-                    </Text>
+                <View style={{ flex: 2 }}>
+                    <Image source={{ uri: this.props.filmUri }} style={styles.img} />
                 </View>
-                <View style={{flex: 2}}>
-                    <Image source={{ uri: this.props.film }} style={styles.img}/>
-                </View>
-                 */}
-            </View>  
+            </View>
         )
     }
 }
 
-{/*
+
 const styles = StyleSheet.create({
     movie: {
-        height: 130,
-        width: 130,
+        height: 150,
+        width: 90,
         borderWidth: 1,
-        marginLeft: 20,
+        marginLeft: 8,
+        marginTop: 23,
     },
-}) */}
+    types: {
+        top: 20,
+    },
+    text: {
+        color: '#fff',
+        fontFamily: 'Arial',
+        fontSize: 16,
+    },
+    img: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'cover',
+    },
+})
 
