a4eddaf3e3327809ed3e5d647dd4378d7f666b7e
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-02-24 22:58:22 -0300

    Update App.js

--- App.js
+++ App.js
@@ -9,42 +9,43 @@ import {
   TouchableOpacity,
 } from 'react-native'
 import Play from './components/Play'
+import Movies from './components/Movies'
 
 export default class App extends Component {
   render() {
     return (
       <ScrollView style={styles.container}>
-       <View style={{paddingTop: 23}}>
-        <Image style={styles.main_star} source={{ uri: 'https://github.com/KalleHallden/Netflix_Clone/blob/master/netflix_clone/lib/assets/starwars1.jpg?raw=true'}}/>
+        <View style={{  }}>
+          <Image style={styles.main_star} source={{ uri: 'https://i.pinimg.com/originals/88/eb/2d/88eb2df9b2a2a547f093646d3149dfef.jpg' }} />
           <View style={styles.nav}>
-
-            <View style={{marginTop: -12}}>
-              <Image source={{ uri: 'https://i.pinimg.com/originals/60/b9/b1/60b9b11b28fde9cc13dd039ce8514076.png' }} style={{width: 50, height:50}}/>
+            <View style={{ marginTop: -12 }}>
+              <Image source={{ uri: 'https://i.pinimg.com/originals/60/b9/b1/60b9b11b28fde9cc13dd039ce8514076.png' }} style={{ width: 50, height: 50 }} />
             </View>
-
-            <TouchableOpacity style={{left: 10, fontFamily: 'Arial'}}>
-              <Text style={{ color: '#F5FCFF'}}>
-                Series 
+            <TouchableOpacity style={{ left: 10, fontFamily: 'Arial' }}>
+              <Text style={{ color: '#fff' }}>
+                Series
               </Text>
             </TouchableOpacity>
-
-            <TouchableOpacity style={{ left: 40, fontFamily: 'Arial'}}>
-              <Text style={{ color: '#F5FCFF'}}>
+            <TouchableOpacity style={{ left: 40, fontFamily: 'Arial' }}>
+              <Text style={{ color: '#fff' }}>
                 Films
               </Text>
             </TouchableOpacity>
-
-            <TouchableOpacity style={{ left: 70, fontFamily: 'Arial'}}>
-              <Text style={{ color: '#F5FCFF'}}>
+            <TouchableOpacity style={{ left: 70, fontFamily: 'Arial' }}>
+              <Text style={{ color: '#fff' }}>
                 My List
               </Text>
             </TouchableOpacity>
-
           </View>
         </View>
-
         <Play />
+        {/*<View style={styles.filmList}>
+          <ScrollView horizontal={true}>
 
+              <Movies />
+
+          </ScrollView>
+        </View> */}
 
 
 
@@ -57,15 +58,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#181818',
-   // backgroundColor: '#F5FCFF',
+    // backgroundColor: '#F5FCFF',
   },
   main_star: {
-    height: 310,
+    height: 340,
   },
   nav: {
     flexDirection: 'row',
-    marginTop: -300,
+    marginTop: -303,
   },
- 
+  filmList: {
+    borderWidth: 1,
+    marginTop: -45,
+  },
+
 })
 
