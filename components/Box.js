b8215211ccebe38895d5b8adead011477633f579
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-03-01 00:12:19 -0300

    Update Box.js

--- components/Box.js
+++ components/Box.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    ScrollView,
-    TouchableOpacity,
-} from 'react-native'
-
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import { Icon } from 'react-native-elements';
 
 export default class App extends Component {
-    render() {
-        return (
-            <View style={styles.box}>
-
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.box}>
+        <Text style={styles.episode}>{this.props.infor}</Text>
+        <View style={styles.information}>
+          <Icon type="font-awesome" name="info-circle" size={20} color="#fff" />
+        </View>
+      </View>
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
-    cont: {
-        height: 120,
-        width: 80,
-        borderWidth: 1,
-        marginLeft: 8,
-        marginTop: 23,
-    },
-    images: {
-        flex: 1,
-        width: null,
-        height: null,
-        resizeMode: 'cover',
-    },
-    box: {
-        borderWidth: 1,   
-        marginTop: 150,
-        height: 20,
-        width: 100,
-    },
-})
-
+  box: {
+    borderWidth: 1,
+    marginTop: 136,
+    marginLeft: -110,
+    height: 30,
+    width: 110,
+    backgroundColor: '#000',
+  },
+  episode: {
+    color: '#fff',
+    left: 8,
+    top: 5,
+  },
+  information: {
+    left: 35,
+    bottom: 13,
+  },
+});
