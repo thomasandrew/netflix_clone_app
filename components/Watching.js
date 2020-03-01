595a9df53c4056c9ed6e846aa53b0dc586062221
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-03-01 00:14:19 -0300

    Update Watching.js

--- components/Watching.js
+++ components/Watching.js
@@ -1,50 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    ScrollView,
-    TouchableOpacity,
-} from 'react-native'
-
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 
 export default class App extends Component {
-    render() {
-        return (
-            <View style={styles.cont}>
-                <View style={{ flex: 2 }}>
-                    {/*<Image source={{ uri: this.props.wacthUri }} style={styles.images} /> */}
-                    <Image source={{ uri: 'https://i.pinimg.com/564x/e2/15/5b/e2155ba33358d3a4f46ca6d23cde7fde.jpg' }} style={styles.images} />
-                </View>
-
-                <View style={styles.box}>
-                   
-                </View>
-                
-           
-
-
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.cont}>
+        <View style={{ flex: 2 }}>
+          <Image source={{ uri: this.props.conti }} style={styles.images} />
+        </View>
+      </View>
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
-    cont: {
-        height: 120,
-        width: 80,
-        borderWidth: 1,
-        marginLeft: 8,
-        marginTop: 23,
-    },
-    images: {
-        flex: 1,
-        width: null,
-        height: null,
-        resizeMode: 'cover',
-    },
-})
-
+  cont: {
+    height: 140,
+    width: 110,
+    borderWidth: 1,
+    marginLeft: 17,
+    marginTop: -7,
+  },
+  images: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+});
