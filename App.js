ab65f6d129f2b17722b4e938953b8dd56a697763
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-03-04 22:33:02 -0300

    Update App.js

--- App.js
+++ App.js
@@ -11,97 +11,95 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { MaterialCommunityIcons } from 'react-native-vector-icons';
-import { Icon } from 'react-native-elements'
-import HomeS from './components/HomeS'
+import { Icon } from 'react-native-elements';
+import HomeS from './components/HomeS';
 
 function HomeScreen() {
-  return (
-     <HomeS />
-  )
+  return <HomeS />;
 }
 
 function SearchScreen() {
-    return (
+  return (
     <View style={styles.search}>
-     <Text style={{ fontSize: 25, color: '#fff' }}>Coming Soon</Text>
+      <Text style={{ fontSize: 25, color: '#fff' }}>Coming Soon</Text>
     </View>
-  )
+  );
 }
 
 function DownloadScreen() {
-    return (
+  return (
     <View style={styles.download}>
-     <Text style={{ fontSize: 25, color: '#fff' }}>Coming Soon</Text>
+      <Text style={{ fontSize: 25, color: '#fff' }}>Coming Soon</Text>
     </View>
-  )
+  );
 }
 
 function MoreScreen() {
-    return (
+  return (
     <View style={styles.more}>
-     <Text style={{ fontSize: 25, color: '#fff' }}>Coming Soon</Text>
+      <Text style={{ fontSize: 25, color: '#fff' }}>Coming Soon</Text>
     </View>
-  )
+  );
 }
 
-const Tab = createMaterialBottomTabNavigator()
+const Tab = createMaterialBottomTabNavigator();
 
 function MyTabs() {
   return (
-     <Tab.Navigator
-       barStyle={{ backgroundColor: '#000' }}
-     >
-
-      <Tab.Screen 
-        name="Home" 
-        component={HomeScreen} 
+    <Tab.Navigator barStyle={{ backgroundColor: '#000' }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
         options={{
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={25} />   
+            <MaterialCommunityIcons name="home" color={color} size={25} />
           ),
         }}
-        />
-
-      <Tab.Screen 
-        name="Search" 
-        component={SearchScreen} 
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon type="font-awesome" name="search" size={23} color={color} />   
-          ),
-        }} 
-        />
-
-      <Tab.Screen 
-        name="Download" 
-        component={DownloadScreen} 
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="download-multiple" color={color} size={25} />   
-          ),
-        }}
-        />
-
-      <Tab.Screen 
-      name="More" 
-      component={MoreScreen} 
-      options={{
-        tabBarIcon: ({ color, size }) => (
-           <Icon type="font-awesome" name="list" size={23} color={color} />   
-        ),
-      }}
       />
 
-     </Tab.Navigator>
-  )
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon type="font-awesome" name="search" size={23} color={color} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Download"
+        component={DownloadScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="download-multiple"
+              color={color}
+              size={25}
+            />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="More"
+        component={MoreScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon type="font-awesome" name="list" size={23} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
 
 export default function App() {
   return (
     <NavigationContainer>
-       <MyTabs />
+      <MyTabs />
     </NavigationContainer>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -109,13 +107,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'red'
+    backgroundColor: 'red',
   },
   download: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'blue'
+    backgroundColor: 'blue',
   },
   more: {
     flex: 1,
